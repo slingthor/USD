57223950bee9f8e3dce068d1def458fbfb82d54a
Jason Fielder <jason.fielder@apple.com>
2018-10-21 20:25:52 +0100

    Implemented metal backing for udim textures

--- pxr/imaging/lib/mtlf/udimTexture.cpp
+++ pxr/imaging/lib/mtlf/udimTexture.cpp
@@ -24,6 +24,7 @@
 /// \file garch/udimTexture.cpp
 
 #include "pxr/imaging/glf/glew.h"
+#include "pxr/imaging/mtlf/mtlDevice.h"
 
 #include "pxr/imaging/mtlf/udimTexture.h"
 
@@ -77,54 +78,73 @@ MtlfUdimTexture::_CreateGPUResources(unsigned int numChannels,
                                      std::vector<float> &layoutData)
 {
     unsigned int mipCount = mips.size();
+    size_t pixelByteSize;
     MTLPixelFormat internalFormat = MTLPixelFormatRGBA8Snorm;
+
     if (type == GL_FLOAT) {
         constexpr MTLPixelFormat internalFormats[] =
         { MTLPixelFormatR32Float, MTLPixelFormatRG32Float, MTLPixelFormatRGBA32Float, MTLPixelFormatRGBA32Float };
+        pixelByteSize = 4 * numChannels;
         internalFormat = internalFormats[numChannels - 1];
     } else if (type == GL_UNSIGNED_SHORT) {
         constexpr MTLPixelFormat internalFormats[] =
         { MTLPixelFormatR16Unorm, MTLPixelFormatRG16Unorm, MTLPixelFormatRGBA16Unorm, MTLPixelFormatRGBA16Unorm };
+        pixelByteSize = 2 * numChannels;
         internalFormat = internalFormats[numChannels - 1];
     } else if (type == GL_HALF_FLOAT_ARB) {
         constexpr MTLPixelFormat internalFormats[] =
         { MTLPixelFormatR16Float, MTLPixelFormatRG16Float, MTLPixelFormatRGBA16Float, MTLPixelFormatRGBA16Float };
+        pixelByteSize = 2 * numChannels;
         internalFormat = internalFormats[numChannels - 1];
     } else if (type == GL_UNSIGNED_BYTE) {
         constexpr MTLPixelFormat internalFormats[] =
         { MTLPixelFormatR8Unorm, MTLPixelFormatRG8Unorm, MTLPixelFormatRGBA8Unorm, MTLPixelFormatRGBA8Unorm };
+        pixelByteSize = numChannels;
         internalFormat = internalFormats[numChannels - 1];
     }
 
-    TF_FATAL_CODING_ERROR("Not Implemented");
-    /*
-    glGenTextures(1, &_imageArray);
-    glBindTexture(GL_TEXTURE_2D_ARRAY, _imageArray);
-    glTexStorage3D(GL_TEXTURE_2D_ARRAY,
-        mipCount, internalFormat,
-        _width, _height, _depth);
-
+    id<MTLDevice> device = MtlfMetalContext::GetMetalContext()->device;
+    
+    MTLTextureDescriptor* descImage =
+        [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormat(internalFormat)
+                                                           width:_width
+                                                          height:_height
+                                                       mipmapped:YES];
+    descImage.textureType = MTLTextureType2DArray;
+    descImage.arrayLength = _depth;
+    descImage.mipmapLevelCount = mipCount;
+    descImage.resourceOptions = MTLResourceStorageModeManaged;
+    _imageArray = [device newTextureWithDescriptor:descImage];
+    
     for (int mip = 0; mip < mipCount; ++mip) {
         _TextureSize const& mipSize = mips[mip];
-        glTexSubImage3D(GL_TEXTURE_2D_ARRAY, mip, 0, 0, 0,
-                        mipSize.width, mipSize.height, _depth, _format, type,
-                        mipData[mip].data());
+
+        for (int slice = 0; slice < _depth; slice++) {
+            uint8_t const *sliceBase = static_cast<uint8_t const*>(mipData[mip].data()) +
+                pixelByteSize * mipSize.width * mipSize.height * slice;
+
+            [_imageArray replaceRegion:MTLRegionMake2D(0, 0, mipSize.width, mipSize.height)
+                           mipmapLevel:mip
+                                 slice:slice
+                             withBytes:sliceBase
+                           bytesPerRow:pixelByteSize * mipSize.width
+                         bytesPerImage:0];
+        }
     }
+    
+    MTLTextureDescriptor* descLayout =
+        [MTLTextureDescriptor texture2DDescriptorWithPixelFormat:MTLPixelFormatR32Float
+                                                           width:layoutData.size()
+                                                          height:1
+                                                       mipmapped:NO];
+    descLayout.textureType = MTLTextureType1D;
+    descLayout.resourceOptions = MTLResourceStorageModeManaged;
+    _layout = [device newTextureWithDescriptor:descLayout];
 
-    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
-
-    glGenTextures(1, &_layout);
-    glBindTexture(GL_TEXTURE_1D, _layout);
-    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_1D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexImage1D(GL_TEXTURE_1D, 0, GL_R32F, layoutData.size(), 0,
-        GL_RED, GL_FLOAT, layoutData.data());
-    glBindTexture(GL_TEXTURE_1D, 0);*/
+    [_layout replaceRegion:MTLRegionMake1D(0, layoutData.size())
+               mipmapLevel:0
+                 withBytes:layoutData.data()
+               bytesPerRow:layoutData.size() * sizeof(float)];
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
