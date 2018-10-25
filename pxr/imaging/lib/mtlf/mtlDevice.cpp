e5786fb29cb0aa16dc56487f28624af2258821f3
Jeroen Soethoudt <jsoethoudt@apple.com>
2018-10-25 14:04:34 +0100

    Disable GS manual hazard tracking due to issues

--- pxr/imaging/lib/mtlf/mtlDevice.cpp
+++ pxr/imaging/lib/mtlf/mtlDevice.cpp
@@ -51,7 +51,7 @@
 
 //Be careful with these when using Concurrent Dispatch. Also; you can't enable asynchronous compute without also enabling METAL_EVENTS_AVAILABLE.
 #define METAL_COMPUTEGS_BUFFER_REUSE                      1
-#define METAL_COMPUTEGS_MANUAL_HAZARD_TRACKING            1
+#define METAL_COMPUTEGS_MANUAL_HAZARD_TRACKING            0
 #define METAL_COMPUTEGS_ALLOW_ASYNCHRONOUS_COMPUTE        0     //Currently disabled due to additional overhead of using events to synchronize. 
 
 PXR_NAMESPACE_OPEN_SCOPE
