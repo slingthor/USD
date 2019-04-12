a2fcb5ebf4a6a39f947d65425af6fc7b258feb28
Jason Fielder <jason.fielder@apple.com>
2019-04-12 15:09:29 +0100

    Disable clang warning about unrecognised warning options

--- cmake/defaults/clangdefaults.cmake
+++ cmake/defaults/clangdefaults.cmake
@@ -44,6 +44,7 @@ _disable_warning("reinterpret-base-class")
 _disable_warning("shorten-64-to-32")
 _disable_warning("unguarded-availability-new")
 _disable_warning("self-assign-overloaded")
+_disable_warning("unknown-warning-option")
 
 if (APPLEIOS)
 	_disable_warning("comma")
