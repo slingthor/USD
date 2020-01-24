5bcfda2eb83944b323ece7fd2314f6b323b2c965
davidgyu <davidgyu@users.noreply.github.com>
2020-01-24 10:57:55 -0800

    [HdSt,Hdx] Fixed glew.h include dependencies
    Removed includes of glew.h from headers in favor of
    garch/gl.h and also unnecessary includes of glew.h
    
    (Internal change: 2036715)
    (Internal change: 2036745)

--- pxr/imaging/hdSt/mesh.cpp
+++ pxr/imaging/hdSt/mesh.cpp
@@ -22,7 +22,6 @@
 // language governing permissions and limitations under the Apache License.
 //
 #include "pxr/pxr.h"
-#include "pxr/imaging/glf/glew.h"
 
 #include "pxr/imaging/garch/contextCaps.h"
 #include "pxr/imaging/garch/resourceFactory.h"
