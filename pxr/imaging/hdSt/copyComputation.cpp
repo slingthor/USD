PixarAnimationStudios/OpenUSD

[hd, hdSt] Improve the BAR interface for querying offsets and fix copy computation for interleaved resources.
When querying offsets in a BAR, we may care about the byte offset at which the BAR begins for a resource, or just the element offset in the underlying buffer array. The existing GetOffset API returned the element offset for VBO BARs and byte offset for interleaved UBO/SSBO BARs, and did not robustly handle migrating data via the GPU copy computation for interleaved BARs.

- Remove GetIndex()
- Replace GetOffset() with GetElementOffset() and GetByteOffset(resource). Fix clients to call the appropriate function.
- Update copyComputation to handle fixed size arrays (by factoring the tupleType's count member).

(Internal change: 2039755)

pxr/imaging/hdSt/copyComputation.cpp
@@ -23,6 +23,7 @@
 //
 #include "pxr/imaging/glf/glew.h"
 #include "pxr/imaging/glf/contextCaps.h"
+#include "pxr/imaging/glf/diagnostic.h"
 
 #include "pxr/imaging/hdSt/copyComputation.h"
 #include "pxr/imaging/hdSt/bufferArrayRangeGL.h"
@@ -55,37 +56,37 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
 
     HdStBufferArrayRangeGLSharedPtr srcRange =
         boost::static_pointer_cast<HdStBufferArrayRangeGL> (_src);
-    HdStBufferArrayRangeGLSharedPtr range =
+    HdStBufferArrayRangeGLSharedPtr dstRange =
         boost::static_pointer_cast<HdStBufferArrayRangeGL> (range_);
 
-    HdStBufferResourceGLSharedPtr src = srcRange->GetResource(_name);
-    HdStBufferResourceGLSharedPtr dst = range->GetResource(_name);
+    HdStBufferResourceGLSharedPtr srcRes = srcRange->GetResource(_name);
+    HdStBufferResourceGLSharedPtr dstRes = dstRange->GetResource(_name);
 
-    if (!TF_VERIFY(src)) {
+    if (!TF_VERIFY(srcRes)) {
         return;
     }
-    if (!TF_VERIFY(dst)) {
-        return;
-    }
-
-    // XXX:Arrays: Should this support array-valued types?
-    // Commented-out version that would support arrays:
-    // int srcBytesPerElement = HdDataSizeOfTupleType(src->GetTupleType());
-    // int dstBytesPerElement = HdDataSizeOfTupleType(dst->GetTupleType());
-    int srcBytesPerElement = HdDataSizeOfType(src->GetTupleType().type);
-    int dstBytesPerElement = HdDataSizeOfType(dst->GetTupleType().type);
-
-    if (!TF_VERIFY(srcBytesPerElement == dstBytesPerElement)) {
+    if (!TF_VERIFY(dstRes)) {
         return;
     }
 
-    GLintptr readOffset = _src->GetOffset() *  srcBytesPerElement;
-    GLintptr writeOffset = range->GetOffset() * dstBytesPerElement;
-    GLsizeiptr copySize = _src->GetNumElements() * srcBytesPerElement;
+    int srcResSize = HdDataSizeOfTupleType(srcRes->GetTupleType()) *
+                     srcRange->GetNumElements();
+    int dstResSize = HdDataSizeOfTupleType(dstRes->GetTupleType()) *
+                     dstRange->GetNumElements();
 
-    if (!TF_VERIFY(_src->GetNumElements() <= range->GetNumElements())) {
-         return;
+    if (!TF_VERIFY(srcResSize <= dstResSize)) {
+        // The number of elements in the BAR *can* differ during migration.
+        // One example is during mesh refinement when migration is necessary,
+        // and we copy only the unrefined data over.
+        TF_CODING_ERROR("Migration error for %s: Source resource (%d) size is "
+            "larger than destination resource size (%d)\n",
+            _name.GetText(), srcResSize, dstResSize);
+        return;
     }
+
+    GLintptr readOffset = srcRange->GetByteOffset(_name) + srcRes->GetOffset();
+    GLintptr writeOffset = dstRange->GetByteOffset(_name) + dstRes->GetOffset();
+    GLsizeiptr copySize = srcResSize;
 
     GlfContextCaps const &caps = GlfContextCaps::GetInstance();
 
@@ -98,8 +99,8 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
         // be allocated, so the check for resource allocation has been moved
         // until after the copy size check.
 
-        GLint srcId = src->GetId();
-        GLint dstId = dst->GetId();
+        GLint srcId = srcRes->GetId();
+        GLint dstId = dstRes->GetId();
 
         if (!TF_VERIFY(srcId)) {
             return;
@@ -123,6 +124,8 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
             glBindBuffer(GL_COPY_WRITE_BUFFER, 0);
         }
     }
+
+    GLF_POST_PENDING_GL_ERRORS();
 }
 
 int

slingthor/USD

pxr/imaging/hdSt/copyComputation.cpp
@@ -26,6 +26,8 @@
 #include "pxr/imaging/garch/contextCaps.h"
 #include "pxr/imaging/garch/resourceFactory.h"
 
+#include "pxr/imaging/glf/diagnostic.h"
+
 #include "pxr/imaging/hdSt/copyComputation.h"
 #include "pxr/imaging/hdSt/bufferResource.h"
 #include "pxr/imaging/hd/bufferArrayRange.h"
@@ -57,37 +59,37 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
 
     HdBufferArrayRangeSharedPtr srcRange =
         boost::static_pointer_cast<HdBufferArrayRange> (_src);
-    HdBufferArrayRangeSharedPtr range =
+    HdBufferArrayRangeSharedPtr dstRange =
         boost::static_pointer_cast<HdBufferArrayRange> (range_);
 
-    HdBufferResourceSharedPtr src = srcRange->GetResource(_name);
-    HdBufferResourceSharedPtr dst = range->GetResource(_name);
+    HdBufferResourceSharedPtr srcRes = srcRange->GetResource(_name);
+    HdBufferResourceSharedPtr dstRes = dstRange->GetResource(_name);
 
-    if (!TF_VERIFY(src)) {
+    if (!TF_VERIFY(srcRes)) {
         return;
     }
-    if (!TF_VERIFY(dst)) {
-        return;
-    }
-
-    // XXX:Arrays: Should this support array-valued types?
-    // Commented-out version that would support arrays:
-    // int srcBytesPerElement = HdDataSizeOfTupleType(src->GetTupleType());
-    // int dstBytesPerElement = HdDataSizeOfTupleType(dst->GetTupleType());
-    int srcBytesPerElement = HdDataSizeOfType(src->GetTupleType().type);
-    int dstBytesPerElement = HdDataSizeOfType(dst->GetTupleType().type);
-
-    if (!TF_VERIFY(srcBytesPerElement == dstBytesPerElement)) {
+    if (!TF_VERIFY(dstRes)) {
         return;
     }
 
-    GLintptr readOffset = _src->GetOffset() *  srcBytesPerElement;
-    GLintptr writeOffset = range->GetOffset() * dstBytesPerElement;
-    GLsizeiptr copySize = _src->GetNumElements() * srcBytesPerElement;
+    int srcResSize = HdDataSizeOfTupleType(srcRes->GetTupleType()) *
+                     srcRange->GetNumElements();
+    int dstResSize = HdDataSizeOfTupleType(dstRes->GetTupleType()) *
+                     dstRange->GetNumElements();
 
-    if (!TF_VERIFY(_src->GetNumElements() <= range->GetNumElements())) {
-         return;
+    if (!TF_VERIFY(srcResSize <= dstResSize)) {
+        // The number of elements in the BAR *can* differ during migration.
+        // One example is during mesh refinement when migration is necessary,
+        // and we copy only the unrefined data over.
+        TF_CODING_ERROR("Migration error for %s: Source resource (%d) size is "
+            "larger than destination resource size (%d)\n",
+            _name.GetText(), srcResSize, dstResSize);
+        return;
     }
+
+    GLintptr readOffset = srcRange->GetByteOffset(_name) + srcRes->GetOffset();
+    GLintptr writeOffset = dstRange->GetByteOffset(_name) + dstRes->GetOffset();
+    GLsizeiptr copySize = srcResSize;
 
     GarchContextCaps const &caps = GarchResourceFactory::GetInstance()->GetContextCaps();
 
@@ -103,8 +105,8 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
         // Create a virtual copy method on the ArrayRange object to do the below block
         TF_FATAL_CODING_ERROR("Not Implemented");
         {/*
-            GLint srcId = src->GetId();
-            GLint dstId = dst->GetId();
+        	GLint srcId = srcRes->GetId();
+        	GLint dstId = dstRes->GetId();
 
             if (!TF_VERIFY(srcId)) {
                 return;
@@ -129,6 +131,8 @@ HdStCopyComputationGPU::Execute(HdBufferArrayRangeSharedPtr const &range_,
             }*/
         }
     }
+
+    GLF_POST_PENDING_GL_ERRORS();
 }
 
 int