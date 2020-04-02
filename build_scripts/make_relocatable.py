7356cece655f91d0221c060a39f8e4f4d566b88f
Jason Fielder <jason.fielder@apple.com>
2020-04-02 18:26:56 +0100

    Modified make_relocatable to more deeply sign binaries

--- build_scripts/make_relocatable.py
+++ build_scripts/make_relocatable.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import subprocess
+import threading
 from distutils.dir_util import copy_tree
 from os.path import isdir, isfile, join
 from shutil import copy, copyfile
@@ -17,6 +18,14 @@ elif SDKVersion >= "11.0" and codeSignIDs.find("Apple Development") != -1:
 elif codeSignIDs.find("Mac Developer") != -1:
     codeSignID = "Mac Developer"
 
+# Validate that we have a codesign ID that both exists and isn't ambiguous
+if codeSignIDs.count(codeSignID) != 1:
+    print("Codesign Error: Unable to identify signing identity. " +
+        "Pleas specify by setting the XCODE_ATTRIBUTE_CODE_SIGN_ID environment " +
+        "variable to the one you'd like to use. Options are:\n" + codeSignIDs +
+        "Skipping make_relocatable")
+    codeSignID = None
+
 devout = open(os.devnull, 'w')
 
 def extract_files(path, cond, files):
@@ -28,32 +37,55 @@ def extract_files(path, cond, files):
             files.append(os.path.join(path, file))
 
 
-
 def extract_files_recursive(path, cond, files):
     # r=root, d=directories, f = files
-    for r, d, f in os.walk(path):
+    for r, d, f in os.walk(path, topdown=False):
         for file in f:
-            if cond(path+file):
-                files.append(os.path.join(r, file))
+            name = os.path.join(r, file)
+            if cond(name):
+                # print("Adding: " + name)
+                files.append(name)
+        # .frameworks are directories - which are valid as codesigning targets
+        for directory in d:
+            name = os.path.join(r, directory)
+            if cond(name):
+                files.append(name)
 
+def Run(cmd):
+    subprocess.call(cmd, stdout=devout, stderr=devout)
 
 
 def add_rpath_to_files(path, files):
+    threads = []
     for f in files:
         dir_path=os.path.dirname(f)
         path_between = os.path.relpath(path, dir_path)
         if path_between != ".":
-            subprocess.call(['install_name_tool', '-add_rpath', '@loader_path/' + path_between, f],
-                stdout=devout, stderr=devout)
+            t = threading.Thread(target=Run, args=(['install_name_tool', '-add_rpath', '@loader_path/' + path_between, f],))
+#            subprocess.call(['install_name_tool', '-add_rpath', '@loader_path/' + path_between, f],
+#                stdout=devout, stderr=devout)
+            t.start()
+            threads.append(t)
+
+    for t in threads:
+        t.join()
 
 
 def remove_rpath(path, files):
+    threads = []
     for f in files:
-        subprocess.call(['install_name_tool', '-delete_rpath', path, f],
-            stdout=devout, stderr=devout)
+#        subprocess.call(['install_name_tool', '-delete_rpath', path, f],
+#            stdout=devout, stderr=devout)
+        t = threading.Thread(target=Run, args=(['install_name_tool', '-delete_rpath', path, f],))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
 
 
 def change_absolute_to_relative(files, path_to_replace, custom_path=""):
+    threads = []
     for f in files:
         otool_output = ""
 
@@ -74,17 +106,32 @@ def change_absolute_to_relative(files, path_to_replace, custom_path=""):
                     inside_idx = replace_path_idx + len(path_to_replace)
                     suffix_path = extracted_path[inside_idx:]
                     path_between = os.path.relpath(custom_path+suffix_path, cur_dir_path)
-                subprocess.call(['install_name_tool', '-change', extracted_path, '@loader_path/' + path_between, f],
-                    stdout=devout, stderr=devout)
+#                subprocess.call(['install_name_tool', '-change', extracted_path, '@loader_path/' + path_between, f],
+#                    stdout=devout, stderr=devout)
+                t = threading.Thread(target=Run, args=(['install_name_tool', '-change', extracted_path, '@loader_path/' + path_between, f],))
+                t.start()
+                threads.append(t)
+
+    for t in threads:
+        t.join()
 
 
 def codesign_files(files):
+    threads = []
     for f in files:
-        subprocess.call(['codesign', '-f', '-s', '{codesignid}'.format(codesignid=codeSignID), f],
-            stdout=devout, stderr=devout)
+        t = threading.Thread(target=Run, args=(['codesign', '-f', '-s', '{codesignid}'.format(codesignid=codeSignID), f],))
+        t.start()
+        threads.append(t)
+#        subprocess.call(['codesign', '-f', '-s', '{codesignid}'.format(codesignid=codeSignID), f],
+#            stdout=devout, stderr=devout)
+
+    for t in threads:
+        t.join()
 
 
 def is_object_file(file):
+    if os.path.isdir(file):
+        return False
     first_line = open(file).readline().rstrip()
     return not first_line.startswith("#!")
 
@@ -107,6 +154,9 @@ def replace_string_in_file(path, old_string, new_string):
 
 def make_relocatable(install_path, buildPython, iOS, verbose_output=False):
     files = []
+    if codeSignID == None:
+        return
+
     if verbose_output:
         global devout
         devout = sys.stdout
@@ -122,26 +172,6 @@ def make_relocatable(install_path, buildPython, iOS, verbose_output=False):
     #path of the usd repo folder
     file_path = os.path.abspath(__file__)
     src_path  = os.path.abspath(os.path.join(file_path,"../..")) 
-    
-    extract_files_recursive(install_path + '/bin/', is_object_file, files)
-    extract_files_recursive(install_path + '/lib/', (lambda file: '.so' in file or '.dylib' in file), files)
-    extract_files_recursive(install_path + '/plugin/', (lambda file: '.so' in file or '.dylib' in file), files)
-    extract_files_recursive(install_path + '/share/', (lambda file: '.so' in file or '.dylib' in file), files)
-    extract_files_recursive(install_path + '/tests', (lambda file: '.so' in file or '.dylib' in file), files)
-    extract_files(install_path + '/tests', (lambda file: isfile(file) and open(file).readline().rstrip()[0] != "#"), files)
-
-    remove_rpath(install_path + '/lib', files)
-    add_rpath_to_files(install_path + '/lib/', files)
-    change_absolute_to_relative(files, install_path)
-    codesign_files(files)
-
-    ctest_files = []
-    extract_files_recursive(install_path + '/build/', (lambda file: 'CTestTestfile' in file), ctest_files)
-    for file in ctest_files:
-        replace_string_in_file(file, install_path , "$ENV{USD_BUILD}")
-        replace_string_in_file(file, src_path , "$ENV{USD_BUILD}")
-
-    copy(src_path+"/build_scripts/Hydra.command", install_path+"/Hydra.command")
 
     if buildPython:
         import PySide2
@@ -163,3 +193,31 @@ def make_relocatable(install_path, buildPython, iOS, verbose_output=False):
         copy_tree(src_path + "/lib/python/pyside2uic", install_path+"/lib/python/pyside2uic")
         copy(src_path + "/lib/python/pyside-uic", install_path+"/lib/python/pyside-uic")
         copy(src_path + "/lib/python/pyside2-uic", install_path+"/lib/python/pyside2-uic")
+    
+    def file_check(file):
+        name = os.path.basename(file)
+        return '.so' in name or '.dylib' in name or '.framework' in name
+
+    def file_check_qt_exec(file):
+        return '.framework/Versions/5/Qt' in file        
+
+    extract_files_recursive(install_path + '/bin/', is_object_file, files)
+    extract_files_recursive(install_path + '/lib/python/PySide2/Qt/lib/', file_check_qt_exec, files)
+    extract_files_recursive(install_path + '/lib/', file_check, files)
+    extract_files_recursive(install_path + '/plugin/', file_check, files)
+    extract_files_recursive(install_path + '/share/', file_check, files)
+    extract_files_recursive(install_path + '/tests', file_check, files)
+    extract_files(install_path + '/tests', (lambda file: isfile(file) and open(file).readline().rstrip()[0] != "#"), files)
+
+    remove_rpath(install_path + '/lib', files)
+    add_rpath_to_files(install_path + '/lib/', files)
+    change_absolute_to_relative(files, install_path)
+    codesign_files(files)
+
+    ctest_files = []
+    extract_files_recursive(install_path + '/build/', (lambda file: 'CTestTestfile' in file), ctest_files)
+    for file in ctest_files:
+        replace_string_in_file(file, install_path , "$ENV{USD_BUILD}")
+        replace_string_in_file(file, src_path , "$ENV{USD_BUILD}")
+
+    copy(src_path+"/build_scripts/Hydra.command", install_path+"/Hydra.command")
