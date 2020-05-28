446446627682df3fc4536e8e6e8ea06ea392f4af
Jon Creighton <jcreighton@apple.com>
2020-05-28 08:43:09 +0100

    Using the blitOp, still nothing coming out.

--- pxr/usdImaging/usdAppUtils/frameRecorder.cpp
+++ pxr/usdImaging/usdAppUtils/frameRecorder.cpp
@@ -42,6 +42,7 @@
 #include "pxr/imaging/garch/simpleLight.h"
 #include "pxr/imaging/garch/simpleMaterial.h"
 #include "pxr/imaging/glf/diagnostic.h"
+#include "pxr/imaging/hgi/texture.h"
 #include "pxr/usd/sdf/path.h"
 #include "pxr/usd/usd/prim.h"
 #include "pxr/usd/usd/stage.h"
@@ -50,6 +51,8 @@
 #include "pxr/usd/usdGeom/metrics.h"
 #include "pxr/usd/usdGeom/tokens.h"
 
+#include "pxr/imaging/hgi/blitCmds.h"
+#include "pxr/imaging/hgi/blitCmdsOps.h"
 #include "pxr/imaging/hgiMetal/hgi.h"
 
 #include <string>
@@ -152,50 +155,37 @@ _ComputeCameraToFrameStage(const UsdStagePtr& stage, UsdTimeCode timeCode,
 /// Debug api to output the contents of the draw target to a png file.
 static bool
 _WriteToFile(std::string const & filename,
-             size_t imageWidth,
-             size_t imageHeight)
+             HgiTextureHandle const & textureHandle)
 {
+    constexpr GLenum format = GL_RGBA;
+    constexpr GLenum type = GL_FLOAT;
+    constexpr size_t nelems = 4; // For GL_RGBA
+    constexpr size_t elemsize = sizeof(GLfloat); // For GL_FLOAT
     
-    int nelems = 4, // GL_RGBA
-        elemsize = sizeof(GLfloat),
-        stride = imageWidth * nelems * elemsize,
-        bufsize = imageHeight * stride;
+    const auto &textureDesc = textureHandle.Get()->GetDescriptor();
+    const size_t width = textureDesc.dimensions[0];
+    const size_t height = textureDesc.dimensions[1];
+    const size_t stride = width * nelems * elemsize;
+    const size_t bufsize = height * stride;
     
     std::unique_ptr<char[]> buf(new char[bufsize]);
     
-    {
-        glPushClientAttrib(GL_CLIENT_PIXEL_STORE_BIT);
-
-        glPixelStorei(GL_PACK_ROW_LENGTH, 0);
-        glPixelStorei(GL_PACK_ALIGNMENT, 1);
-        glPixelStorei(GL_PACK_SKIP_PIXELS, 0);
-        glPixelStorei(GL_PACK_SKIP_ROWS, 0);
-
-        GLint restoreBinding, restoreActiveTexture;
-        glGetIntegerv( GL_TEXTURE_BINDING_2D, &restoreBinding );
-        glGetIntegerv( GL_ACTIVE_TEXTURE, & restoreActiveTexture);
-
-        glActiveTexture( GL_TEXTURE0 );
-        glBindTexture( GL_TEXTURE_2D, 0 );
-
-        {
-            TRACE_FUNCTION_SCOPE("glGetTexImage");
-            glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_FLOAT, buf.get());
-        }
-
-        glActiveTexture( restoreActiveTexture );
-        glBindTexture( GL_TEXTURE_2D, restoreBinding );
-
-        glPopClientAttrib();
-    }
+    HgiMetal* hgiMetal = MtlfMetalContext::GetMetalContext()->GetHgi();
+    HgiBlitCmdsUniquePtr const blitCmds = hgiMetal->CreateBlitCmds();
+    HgiTextureGpuToCpuOp copyOp;
+    copyOp.gpuSourceTexture = textureHandle;
+    copyOp.cpuDestinationBuffer = buf.get();
+    copyOp.destinationBufferByteSize = bufsize;
+    blitCmds->CopyTextureGpuToCpu(copyOp);
+    hgiMetal->SubmitCmds(blitCmds.get(), 1);
     
     VtDictionary metadata;
 
     GarchImage::StorageSpec storage;
-    storage.width = imageWidth;
-    storage.height = imageHeight;
-    storage.format = GL_RGBA;
-    storage.type = GL_FLOAT;
+    storage.width = width;
+    storage.height = height;
+    storage.format = format;
+    storage.type = type;
     storage.flipped = true;
     storage.data = buf.get();
 
@@ -355,8 +345,11 @@ UsdAppUtilsFrameRecorder::Record(
             glClearBufferfv(GL_DEPTH, 0, CLEAR_DEPTH);
             _imagingEngine.Render(pseudoRoot, renderParams);
         } while (!_imagingEngine.IsConverged());
+        
+        HgiTextureHandle presentationTexture =
+            _imagingEngine.GetPresentationTextureHandle(HdAovTokens->color);
 
-        return _WriteToFile(outputImagePath, _imageWidth, imageHeight);
+        return _WriteToFile(outputImagePath, presentationTexture);
     }
 }
 
