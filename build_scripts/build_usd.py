8ba9779d476b1f5ffe75c6c0daaf36795e30d655
Codrin Popa <cp15316@my.bristol.ac.uk>
2020-08-12 09:15:39 +0100

    Fixed building Intel TBB universal binaries

--- build_scripts/build_usd.py
+++ build_scripts/build_usd.py
@@ -1133,7 +1133,7 @@ def InstallTBB_LinuxOrMacOS(context, force, buildArgs):
         # location that can be shared by both release and debug USD
         # builds. Plus, the TBB build system builds both versions anyway.
         if context.buildUniversal and SupportsMacOSUniversalBinaries():
-            x86Files = glob.glob("build/*intel64*_release/libtbb*.*")
+            x86Files = glob.glob("build/*x86_64*_release/libtbb*.*")
             armFiles = glob.glob("build/*arm64*_release/libtbb*.*")
             libNames = [os.path.basename(x) for x in x86Files]
             x86Dir = os.path.dirname(x86Files[0])
@@ -1141,7 +1141,7 @@ def InstallTBB_LinuxOrMacOS(context, force, buildArgs):
 
             CreateUniversalBinaries(context, libNames, x86Dir, armDir)
 
-            x86Files = glob.glob(os.getcwd() + "/build/*intel64*_debug/libtbb*.*")
+            x86Files = glob.glob(os.getcwd() + "/build/*x86_64*_debug/libtbb*.*")
             armFiles = glob.glob(os.getcwd() + "/build/*arm64*_debug/libtbb*.*")
             libNames = [os.path.basename(x) for x in x86Files]
             x86Dir = os.path.dirname(x86Files[0])
