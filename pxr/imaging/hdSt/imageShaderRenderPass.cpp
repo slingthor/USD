412302280f7192165815cb5fb92a1eb2f054acee
Jon Creighton <jcreighton@apple.com>
2020-12-29 14:52:43 +0000

    [Radar 72131151] Memory leak when resizing view.

--- pxr/imaging/hdSt/imageShaderRenderPass.cpp
+++ pxr/imaging/hdSt/imageShaderRenderPass.cpp
@@ -183,7 +183,7 @@ HdSt_ImageShaderRenderPass::_Execute(
         if (!HdStResourceFactory::GetInstance()->IsOpenGL()) {
             if (desc.colorTextures.size() || desc.depthTexture) {
                 // Set the render pass descriptor for Mtlf to use with the render encoders
-                MTLRenderPassDescriptor* rpd = context->GetHgi()->renderPassDescriptor;
+                MTLRenderPassDescriptor* rpd = context->GetHgi()->_renderPassDescriptor;
                 
                 MTLPixelFormat colorFormat = MTLPixelFormatInvalid;
                 MTLPixelFormat depthFormat = MTLPixelFormatInvalid;
