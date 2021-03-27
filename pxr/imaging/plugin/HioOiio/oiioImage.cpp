f8ae26fd82f2a43513ad75c391a875e759a3b430
Jason Fielder <jason.fielder@apple.com>
2021-03-27 13:20:15 +0000

    Couple of performance fixes for using references rather than copies in loops

--- pxr/imaging/plugin/HioOiio/oiioImage.cpp
+++ pxr/imaging/plugin/HioOiio/oiioImage.cpp
@@ -791,7 +791,7 @@ HioOIIO_Image::Write(StorageSpec const & storage,
     TypeDesc format = _GetOIIOBaseType(storage.format);
     ImageSpec spec(storage.width, storage.height, nchannels, format);
 
-    for (const std::pair<std::string, VtValue>& m : metadata) {
+    for (const std::pair<const std::string, VtValue>& m : metadata) {
         _SetAttribute(&spec, m.first, m.second);
     }
 
