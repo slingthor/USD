b647cfef29f8b737d1df32d4719aa854f6aab79e
Thor Hjalmarsson <ihjalmarsson@apple.com>
2023-10-06 19:32:07 +0100

    correctly set cull mode

--- pxr/imaging/hdSt/pipelineDrawBatch.cpp
+++ pxr/imaging/hdSt/pipelineDrawBatch.cpp
@@ -1671,8 +1671,17 @@ HdSt_PipelineDrawBatch::ExecuteDraw(
     // execute it here.  Otherwise render with the normal graphicsCmd path.
     //
     if (_indirectCommands) {
+        const std::function<void (HgiGraphicsCmds*)> dynamicStateCallback =
+            [this, &renderPassState](HgiGraphicsCmds* cmd) {
+                renderPassState->UpdateDynamicState(
+                        cmd,
+                        _drawItemInstances.front()->GetDrawItem()->
+                                GetGeometricShader());
+        };
         HgiIndirectCommandEncoder *encoder = hgi->GetIndirectCommandEncoder();
-        encoder->ExecuteDraw(gfxCmds, _indirectCommands.get());
+        encoder->ExecuteDraw(gfxCmds,
+                             _indirectCommands.get(),
+                             &dynamicStateCallback);
 
         hgi->DestroyResourceBindings(&(_indirectCommands->resourceBindings));
         _indirectCommands.reset();
@@ -1698,6 +1707,9 @@ HdSt_PipelineDrawBatch::ExecuteDraw(
         
         HgiGraphicsPipelineHandle psoHandle = *pso.get();
         gfxCmds->BindPipeline(psoHandle);
+        renderPassState->UpdateDynamicState(
+                gfxCmds,
+                state.geometricShader);
 
         HgiResourceBindingsDesc bindingsDesc;
         bool const useMeshShaders =
